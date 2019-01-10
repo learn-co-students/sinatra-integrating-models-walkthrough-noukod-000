6b491151d0e36fa68a963453d4993954f59f8dd4
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-10 20:19:25 +0000

    Done.

--- spec/integrating_models_sinatra_walkthrough_spec.rb
+++ spec/integrating_models_sinatra_walkthrough_spec.rb
@@ -5,7 +5,6 @@ describe App do
       get '/'
       expect(last_response.status).to eq(200)
     end
-
     it 'renders form' do
       visit '/'
       expect(page).to have_selector("form")
@@ -13,29 +12,25 @@ describe App do
     end
   end
 
-  describe 'POST /' do
-    it "displays string results" do
-      visit '/'
-
-      fill_in(:user_text, :with => "Green Eggs and Ham")
-      click_button "submit"
-      expect(page.status_code).to eq(200)
-      expect(page).to have_text("Number of Words: 4")
-      expect(page).to have_text("Vowels: 5")
-      expect(page).to have_text("Consonants: 10")
-      expect(page).to have_text("Most Common Letter: G, used 3 times")
-    end
-  end
-
   describe 'TextAnalyzer Class' do
-    let!(:words) { TextAnalyzer.new("mirror mirror on the wall") }
-
+      let!(:words) { TextAnalyzer.new("mirror mirror on the wall") }
     it 'can initialize a new instance of the class' do
       expect(TextAnalyzer.new("hey yall")).to be_an_instance_of(TextAnalyzer)
     end
-
     it 'can have text' do
       expect(words.text).to eq("mirror mirror on the wall")
+    end
+  end
+
+  describe 'POST /' do
+    it "displays string results" do
+      visit '/'
+      fill_in(:user_text, :with => "Green Eggs and Ham")
+      click_button "submit"
+      expect(page).to have_text("Number of Words:4")
+      expect(page).to have_text("Vowels:5")
+      expect(page).to have_text("Consonants:10")
+      expect(page).to have_text("Most Common Letter: G, used 3 times")
     end
   end
 end
